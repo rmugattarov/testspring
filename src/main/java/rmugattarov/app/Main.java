efc8826fad0006fc3287ab6e59b2b64e58681b7d
rmugattarov <mugattarov@gmail.com>
2018-02-10 13:58:56 +0300

    updates

--- src/main/java/rmugattarov/app/Main.java
+++ src/main/java/rmugattarov/app/Main.java
@@ -6,7 +6,7 @@ import rmugattarov.beans.api.StringProvider;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        StringProvider helloStringProvider = (StringProvider) context.getBean("injectStringProvider");
+        StringProvider helloStringProvider = (StringProvider) context.getBean("autowiredStringProvider");
         System.out.println(helloStringProvider.getString());
     }
 }
--- src/main/java/rmugattarov/beans/impl/AutowiredStringProvider.java
+++ src/main/java/rmugattarov/beans/impl/AutowiredStringProvider.java
@@ -2,12 +2,13 @@ package rmugattarov.beans.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import rmugattarov.beans.api.NumeralProvider;
 import rmugattarov.beans.api.StringProvider;
 
+@Component
 public class AutowiredStringProvider implements StringProvider {
     @Autowired
-    @Qualifier("intProvider")
     private NumeralProvider numeralProvider;
 
     public String getString() {
