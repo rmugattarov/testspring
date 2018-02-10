46b900eabd93107a05e1653f8631928521435378
rmugattarov <mugattarov@gmail.com>
2018-02-10 12:02:15 +0300

    updates

--- src/main/java/rmugattarov/beans/impl/AutowiredStringProvider.java
+++ src/main/java/rmugattarov/beans/impl/AutowiredStringProvider.java
@@ -1,11 +1,13 @@
 package rmugattarov.beans.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import rmugattarov.beans.api.NumeralProvider;
 import rmugattarov.beans.api.StringProvider;
 
 public class AutowiredStringProvider implements StringProvider {
     @Autowired
+    @Qualifier("intProvider")
     private NumeralProvider numeralProvider;
 
     public String getString() {
